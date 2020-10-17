3ac34b4606659922d6086a707a60932f12dba4f2
Karishma Saini <ksaini7595@gmail.com>
2020-10-16 23:30:15 -0400

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth , createUserProfileDocument } from './firebase/firebase.utils';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 import './App.css';
+
 
 
 
@@ -25,8 +26,27 @@ class  App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount () {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged( user => {
-      this.setState({ currentUser : user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
+
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+
+          this.setState({
+            currentUser:{
+              id: snapShot.id,
+             ...snapShot.data()
+            }
+          })
+
+        });
+
+      }
+      
+      this.setState({ currentUser: userAuth });
+
+      // this.setState({ currentUser : user });
     });
   }
 
---
+++ src/firebase/header.component.jsx
@@ -0,0 +1,7 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+
+const firestore = firebase.firestore();
+
+firestore.collection('users').doc('7H2fDH8AH02CgH9HUG4u').collection('cartItems').doc('y8S6brtntluoe2vsVUva')
